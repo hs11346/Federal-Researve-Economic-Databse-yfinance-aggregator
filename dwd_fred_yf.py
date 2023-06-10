d966d23b28b04ca7fda26a1a63478ac203348070
Harry See <104689399+hs11346@users.noreply.github.com>
2023-06-11 05:03:51 +0800

    Update dwd_fred_yf.py
    added comments

--- dwd_fred_yf.py
+++ dwd_fred_yf.py
@@ -7,16 +7,14 @@ import warnings
 from datetime import datetime
 warnings.filterwarnings("ignore")
 
-#api is the api key from the fredapi library, which is obtained from the FRED website
-#start is the starting date of the data
-#end is the ending date, by default it is today's date
-#interval is the date interval of the yfinance data
-#please see the readme file for parameters details
+# main function
 def merge_data(api,start,end=datetime.today().strftime('%Y-%m-%d'),interval='1d',yf_code='na',fred_code='na',resamp_freq = 'no',resamp_format = 'last',remove_min="Y"):
     master_dict = {}
     df = pd.DataFrame()
     fred = fd.Fred(api_key=api)
     first = True
+    
+    # put all tickers in a master dictionary, categorised by yfinance or fred
     if yf_code != "na" and fred_code == "na":
         yf_code = yf_code.split(",")
         master_dict = {i:"yfinance" for i in yf_code}
@@ -30,8 +28,9 @@ def merge_data(api,start,end=datetime.today().strftime('%Y-%m-%d'),interval='1d'
         fred_dict = {i:"fred" for i in fred_code}
         yf_dict.update(fred_dict)
         master_dict = yf_dict
-    for code, source in master_dict.items():
-        
+   
+    # get data
+    for code, source in master_dict.items():       
         if source == "fred":
             temp = fred.get_series(code)            
             
@@ -39,22 +38,23 @@ def merge_data(api,start,end=datetime.today().strftime('%Y-%m-%d'),interval='1d'
             time.sleep(1) # To slow down dowload speed from yfinance, adjustable
             ticker = yf.Ticker(code)
             temp = ticker.history(start=start,end=end,interval=interval)
-            
             temp = temp.rename(columns={"Close":code})[code]
         temp.index = pd.to_datetime(temp.index)
         temp = temp[start:end]
         temp.name = code
+        # merge data, first variable is used for checking to see if it is the first iteration in the loop
         if first:
             df = temp
             first = False
         else: 
             df = pd.merge(df,temp,how="outer",left_index=True, right_index=True)      
-
+    # resample frequency
     if resamp_freq != "no":
         if resamp_format == 'last':
             df = df.resample(resamp_freq,base=1).last()
         elif resamp_format == 'first':
             df = df.resample(resamp_freq,base=1).first()
+    # adjust datetime format to remove minute timestamp
     if remove_min == "Y":
         df = df.reset_index()
         df = df.rename(columns={"index":"Date"})
